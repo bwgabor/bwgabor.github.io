cb18d3f3436a02beaed51bb2860e35918aa59797
bwgabor <gabor.sziebert@gmail.com>
2026-07-15 13:47:08 +0200

    refactor: extract analytics scripts into src/js/analytics.js

---
+++ src/js/analytics.js
@@ -0,0 +1,21 @@
+//Google Analytics 4
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag('js', new Date());
+gtag('config', 'G-4W76F35N6G');
+
+//Microsoft Clarity 
+(function (c, l, a, r, i, t, y) {
+  c[a] =
+    c[a] ||
+    function () {
+      (c[a].q = c[a].q || []).push(arguments);
+    };
+  t = l.createElement(r);
+  t.async = 1;
+  t.src = 'https://www.clarity.ms/tag/' + i;
+  y = l.getElementsByTagName(r)[0];
+  y.parentNode.insertBefore(t, y);
+})(window, document, 'clarity', 'script', 'xg81blo0xu');

8fff1ceb2cc8b0803e9a08c2f0900a5208665407
bwgabor <gabor.sziebert@gmail.com>
2026-07-15 13:26:22 +0200

    build: fix robots.txt and sitemap.xml output path via passthrough copy

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,8 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/css');
   eleventyConfig.addPassthroughCopy('src/js');
   eleventyConfig.addPassthroughCopy('src/assets');
+  eleventyConfig.addPassthroughCopy({ 'src/robots.txt': '/robots.txt' });
+  eleventyConfig.addPassthroughCopy({ 'src/sitemap.xml': '/sitemap.xml' });
 
   return {
     dir: {
